b79cea51b5488ed1a768d7a639413c3d390c9868
dbonnin <dbonnin@visionbanco.com>
2024-01-19 17:44:58 -0300

    added my files

---
+++ solution/login.bat
@@ -0,0 +1,8 @@
+rem login.bat
+
+set PROFILE=%1
+
+aws configure set aws_access_key_id    --profile %PROFILE%
+aws configure set aws_secret_access_key   --profile %PROFILE%
+aws configure set region us-east-1 --profile %PROFILE%
+aws configure set output json --profile %PROFILE%
---
+++ solution/run.bat
@@ -0,0 +1,63 @@
+@echo off
+setlocal
+
+set ACTION=%1
+set STACK_NAME=%2
+set TEMPLATE_NAME=%3
+set PARMS_NAME=%4
+
+if "%ACTION%"=="emptyS3" (
+    set S3BUCKET=%2
+    set REGION=%3
+    set PROFILE=%4
+) else (
+    set REGION=%5
+    set PROFILE=%6
+)
+
+echo Execution with: 
+
+echo %ACTION% 
+echo %STACK_NAME%
+echo %TEMPLATE_NAME%
+echo %PARMS_NAME%
+echo %REGION%
+echo %PROFILE%
+
+if "%ACTION%"=="emptyS3" (
+
+    aws s3 rm s3://%S3BUCKET% --recursive --region=%REGION% --profile %PROFILE%
+
+) else if "%ACTION%"=="deploy" (
+
+    aws cloudformation deploy --stack-name %STACK_NAME% ^
+     --template-file %TEMPLATE_NAME% --parameter-overrides file://%PARMS_NAME% --region=%REGION% --profile %PROFILE% --capabilities CAPABILITY_NAMED_IAM
+
+) else if "%ACTION%"=="preview" (
+    
+    aws cloudformation deploy --stack-name %STACK_NAME% ^
+     --template-file %TEMPLATE_NAME% --parameter-overrides file://%PARMS_NAME% --region=%REGION% --profile %PROFILE% --capabilities CAPABILITY_NAMED_IAM --no-execute-changeset
+
+) else if "%ACTION%"=="delete" (
+    
+    aws cloudformation delete-stack --stack-name %STACK_NAME% --region=%REGION% --profile %PROFILE%
+
+) else if "%ACTION%"=="describe" (
+
+    aws cloudformation describe-stacks --stack-name %STACK_NAME% --region=%REGION% --profile %PROFILE%
+
+) else if "%ACTION%"=="list" (
+
+    aws cloudformation list-stacks  --stack-name %STACK_NAME% --region=%REGION% --profile %PROFILE%
+
+) else if "%ACTION%"=="validate" (
+
+    aws cloudformation validate-template --template-body file://%TEMPLATE_NAME% --region=%REGION%  --region=%REGION% --profile %PROFILE% --capabilities CAPABILITY_NAMED_IAM
+
+) else if "%ACTION%"=="get_error" (
+
+    aws cloudformation describe-stack-events --stack-name %STACK_NAME% --region=%REGION% --profile %PROFILE% --capabilities CAPABILITY_NAMED_IAM
+    
+) else (
+    echo "Invalid action"
+)
